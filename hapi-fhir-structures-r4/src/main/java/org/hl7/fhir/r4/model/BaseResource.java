6ef1062d9097c5c45da781b82a8328a8bff2a85c
Sankha <sankha@sdglobaltech.com>
2019-04-12 16:36:49 +0800

    unit code and org codes added

--- hapi-fhir-structures-r4/src/main/java/org/hl7/fhir/r4/model/BaseResource.java
+++ hapi-fhir-structures-r4/src/main/java/org/hl7/fhir/r4/model/BaseResource.java
@@ -5,6 +5,8 @@ import org.hl7.fhir.instance.model.api.IIdType;
 
 import ca.uhn.fhir.context.FhirVersionEnum;
 import ca.uhn.fhir.model.api.IElement;
+import ca.uhn.fhir.model.api.annotation.Child;
+import ca.uhn.fhir.model.api.annotation.Description;
 
 public abstract class BaseResource extends Base implements IAnyResource, IElement {
 
@@ -36,5 +38,129 @@ public abstract class BaseResource extends Base implements IAnyResource, IElemen
     return true;
   }
 
+  /**
+   * unit id
+   */
+  @Child(name = "unitId", type = {StringType.class},  min=0, max=1, modifier=false, summary=true)
+  @Description(shortDefinition="unit id ", formalDefinition="unit id " )
+  protected StringType unitId;
+
+
+  /**
+   * Unit Code
+   */
+  @Child(name = "unitCode", type = {StringType.class},  min=0, max=1, modifier=false, summary=true)
+  @Description(shortDefinition="Unit Code ", formalDefinition="Unit Code " )
+  protected StringType unitCode;
+
+  /**
+   * organisation id
+   */
+  @Child(name = "orgId", type = {StringType.class}, min=0, max=1, modifier=false, summary=true)
+  @Description(shortDefinition="organisation id ",
+    formalDefinition="organisation id " )
+  protected StringType orgId;
+  
+  @Child(name = "orgCode", type = {StringType.class}, min=0, max=1, modifier=false, summary=true)
+  @Description(shortDefinition="organisation code ",
+    formalDefinition="organisation code " )
+  protected StringType orgCode;
+
+
+  /**
+   * for audit purpose this relates to created date and time
+   */
+  @Child(name = "createdDate", type = {DecimalType.class}, min=0, max=1, modifier=false, summary=true)
+  @Description(shortDefinition="for audit purpose this relates to created  date and time",
+    formalDefinition="for audit purpose this relates to created date and time" )
+  protected DecimalType createdDate;
+
+  /**
+   * for audit purpose this relates to updated  date and time.
+   */
+  @Child(name = "updatedDate", type = {DecimalType.class},  min=0, max=1, modifier=false, summary=true)
+  @Description(shortDefinition="for audit purpose this relates to updated  date and time",
+    formalDefinition="for audit purpose this relates to updated  date and time" )
+  protected DecimalType updatedDate;
+
+  /**
+   * for audit purpose this relates to who created this record
+   */
+  @Child(name = "createdBy", type = {DateType.class},  min=0, max=1, modifier=false, summary=true)
+  @Description(shortDefinition="for audit purpose this relates to who created this record",
+    formalDefinition="for audit purpose this relates to who created this record" )
+  protected StringType createdBy;
+
+  /**
+   * for audit purpose this relates to who updated this record.
+   */
+  @Child(name = "updatedBy", type = {StringType.class},  min=0, max=1, modifier=false, summary=true)
+  @Description(shortDefinition="for audit purpose this relates to who updated this record",
+    formalDefinition="for audit purpose this relates to who updated this record" )
+  protected StringType updatedBy;
+
+  public StringType getUnitId() {
+    return unitId;
+  }
+
+  public void setUnitId(StringType unitId) {
+    this.unitId = unitId;
+  }
+
+  public StringType getOrgId() {
+    return orgId;
+  }
+
+  public void setOrgId(StringType orgId) {
+    this.orgId = orgId;
+  }
+
+  public DecimalType getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(DecimalType createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public DecimalType getUpdatedDate() {
+    return updatedDate;
+  }
+
+  public void setUpdatedDate(DecimalType updatedDate) {
+    this.updatedDate = updatedDate;
+  }
+
+  public StringType getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setCreatedBy(StringType createdBy) {
+    this.createdBy = createdBy;
+  }
+
+  public StringType getUpdatedBy() {
+    return updatedBy;
+  }
+
+  public void setUpdatedBy(StringType updatedBy) {
+    this.updatedBy = updatedBy;
+  }
+
+  public StringType getUnitCode() {
+    return unitCode;
+  }
+
+  public void setUnitCode(StringType unitCode) {
+    this.unitCode = unitCode;
+  }
+
+public StringType getOrgCode() {
+	return orgCode;
+}
+
+public void setOrgCode(StringType orgCode) {
+	this.orgCode = orgCode;
+}
 	
 }
